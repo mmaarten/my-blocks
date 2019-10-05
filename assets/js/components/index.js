0a90104c183a54541603fb232bf80a0364a5725a
Maarten Menten <mentenmaarten@gmail.com>
2019-10-05 20:51:06 +0200

    Added post control

--- assets/js/components/index.js
+++ assets/js/components/index.js
@@ -2,4 +2,5 @@
 export { default as URLControl } from './url-control';
 export { default as ImageControl } from './image-control';
 export { default as ImageSizeControl } from './image-size-control';
+export { default as PostControl } from './post-control';
 export { default as Image } from './image';
---
+++ assets/js/components/post-control/index.js
@@ -0,0 +1,83 @@
+import { __ } from '@wordpress/i18n';
+import { Component } from '@wordpress/element';
+import { BaseControl, Button, Icon } from '@wordpress/components';
+import { URLInput } from '@wordpress/block-editor';
+import { withInstanceId } from '@wordpress/compose';
+import classnames from 'classnames';
+
+class PostControl extends Component {
+  constructor() {
+    super( ...arguments );
+
+    this.onChange = this.onChange.bind( this );
+    this.onClear = this.onClear.bind( this );
+
+    this.state = {
+      search: null,
+    }
+  }
+
+  onChange( search, post ) {
+    this.setState( { search : !! post ? post.title : search } );
+    this.props.onChange( post );
+  }
+
+  onClear() {
+    this.setState( { search : null } );
+    this.props.onChange( null );
+  }
+
+  render() {
+    const {
+      search,
+    } = this.state;
+    const {
+      label,
+      hideLabelFromVision,
+      value,
+      help,
+      className,
+      instanceId,
+      onChange,
+    } = this.props;
+
+    const id = `my-post-control-${ instanceId }`;
+
+    return (
+  		<BaseControl
+        label={ label }
+        hideLabelFromVision={ hideLabelFromVision }
+        id={ id }
+        help={ help }
+        className={ classnames( className, 'my-post-control' )  }
+      >
+        { value && (
+          <div className="my-post-control__selected">
+            <input
+              className="block-editor-url-input has-border"
+              type="text"
+              disabled={ true }
+              value={ value.title }
+            />
+            <Button
+              onClick={ this.onClear }
+              isLink={true}
+            ><Icon icon="no-alt" /></Button>
+          </div>
+        ) }
+        { ! value && (
+          <URLInput
+            className="my-post-control__input"
+            id={ id }
+            value={ search }
+            autoFocus={ false }
+            hasBorder
+            onChange={ this.onChange }
+          />
+        ) }
+  		</BaseControl>
+  	);
+  }
+}
+
+export default withInstanceId( PostControl );
