f414ed82a46947cc774ea52e76eb2744438f3916
Maarten Menten <mentenmaarten@gmail.com>
2019-10-13 22:42:45 +0200

    Cleanup

--- assets/js/components/breakpoint-navigation/index.js
+++ assets/js/components/breakpoint-navigation/index.js
@@ -11,9 +11,15 @@ const BreakpointNavigation = ( {
   const controls = [
     {
       icon: 'smartphone',
-      title: __( 'Small Devices' ),
+      title: __( 'Extra Small Devices' ),
       isActive: 'xs' === breakpoint,
       onClick: () => { setState( { breakpoint: 'xs' } ) },
+    },
+    {
+      icon: 'tablet',
+      title: __( 'Small Devices' ),
+      isActive: 'sm' === breakpoint || ! breakpoint,
+      onClick: () => { setState( { breakpoint: 'sm' } ) },
     },
     {
       icon: 'tablet',
@@ -27,6 +33,13 @@ const BreakpointNavigation = ( {
       isActive: 'lg' === breakpoint,
       onClick: () => { setState( { breakpoint: 'lg' } ) },
     },
+    {
+      icon: 'desktop',
+      title: __( 'Extra Large Devices' ),
+      isActive: 'xl' === breakpoint,
+      onClick: () => { setState( { breakpoint: 'xl' } ) },
+    },
+
   ];
 
   return (
