ff55257817b6a3b5fd35aa8d7976822ce4a7fdc9
Maarten Menten <mentenmaarten@gmail.com>
2019-10-19 01:47:43 +0200

    Heading component.

--- assets/js/blocks/heading/index.js
+++ assets/js/blocks/heading/index.js
@@ -1,28 +1,104 @@
-
+import { __ } from '@wordpress/i18n';
 import { Component } from '@wordpress/element';
-import { createHigherOrderComponent } from '@wordpress/compose';
-import { InspectorControls } from '@wordpress/block-editor';
-import { PanelBody } from '@wordpress/components';
+import { createHigherOrderComponent, compose, ifCondition } from '@wordpress/compose';
+import { InspectorControls, withFontSizes, getFontSizeClass } from '@wordpress/block-editor';
+import { PanelBody, FontSizePicker } from '@wordpress/components';
+import { withSelect } from '@wordpress/data';
 import { addFilter } from '@wordpress/hooks';
+import { assign } from 'lodash';
+import classnames from 'classnames';
 
-const withInspectorControls = createHigherOrderComponent( ( BlockEdit ) => {
-    return class WrappedComponent extends Component {
-      constructor() {
-        super( ...arguments );
-      }
-      render() {
-        return (
+const BLOCK_TYPE = 'core/heading';
+
+addFilter( 'blocks.registerBlockType', 'my-blocks/heading/settings', ( settings, name ) => {
+  if ( BLOCK_TYPE === name ) {
+      return settings;
+  }
+
+  assign( settings.attributes, {
+    fontSize : {
+      type: 'string',
+    },
+    customFontSize : {
+      type: 'number',
+    },
+  } );
+
+  return settings;
+} );
+
+
+// addFilter( 'blocks.getSaveElement', 'my-blocks/heading/save', ( element, blockType, attributes ) => {
+//   if ( BLOCK_TYPE !== blockType.name ) {
+//       return element;
+//   }
+//   const { fontSize, customFontSize } = attributes;
+//
+//   const fontSizeClass = getFontSizeClass( fontSize );
+//
+//   const classes = classnames( {
+//     [ fontSizeClass ]: fontSizeClass,
+//     'has-font-size': !! fontSizeClass || !! customFontSize,
+//   } );
+//
+//   const styles = {
+//     fontSize: fontSizeClass ? undefined : customFontSize,
+//   };
+//
+//   return (
+//     <div className={ classes } style={ styles }>
+//       { element }
+//     </div>
+//   );
+//
+// } );
+
+addFilter( 'editor.BlockEdit', 'my-blocks/heading/edit', ( BlockEdit ) => {
+  return compose( [
+    ifCondition( ( { name } ) => BLOCK_TYPE == name ),
+    withFontSizes( 'fontSize' ),
+    withSelect( ( select ) => {
+  		const { getSettings } = select( 'core/block-editor' );
+      const { fontSizes } = getSettings();
+  		return {
+  			fontSizes,
+  		};
+  	} ),
+    () => {
+      return class extends Component {
+        constructor() {
+          super( ...arguments );
+        }
+        render() {
+          const { fontSizes, fontSize, setFontSize, className } = this.props;
+
+          const classes = classnames( {
+						[ fontSize.class ]: fontSize.class,
+            'has-font-size': !! fontSize.class || !! fontSize.size,
+					} );
+
+          const styles = {
+            fontSize: fontSize.size ? fontSize.size + 'px' : undefined,
+          };
+
+          return (
             <>
-              <BlockEdit { ...this.props } />
+              <div className={ classes } style={ styles }>
+                <BlockEdit { ...this.props } />
+              </div>
               <InspectorControls>
-                  <PanelBody>
-                      My custom control
-                  </PanelBody>
+                <PanelBody title={ __( 'Text Settings' ) }>
+      						<FontSizePicker
+                    fontSizes={ fontSizes }
+      							value={ fontSize.size }
+      							onChange={ setFontSize }
+      						/>
+      					</PanelBody>
               </InspectorControls>
             </>
-        );
+          );
+        }
       }
     }
-}, 'withInspectorControl' );
-
-addFilter( 'editor.BlockEdit', 'my-plugin/with-inspector-controls', withInspectorControls );
+  ] )( BlockEdit );
+} );
