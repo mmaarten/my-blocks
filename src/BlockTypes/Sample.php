e6ecc2eb6bf178a0cebb01443411d6c853ca4f41
Maarten Menten <mentenmaarten@gmail.com>
2020-07-04 16:01:09 +0200

    Added Sample block.

---
+++ src/BlockTypes/Sample.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace My\Blocks\BlockTypes;
+
+class Sample extends AbstractBlock
+{
+    /**
+     * Block name.
+     *
+     * @var string
+     */
+    protected $block_name = 'sample';
+}
