f036db0a3a16cc0594e4b5a35371aa4a0db1c4ec
Maarten Menten <>
2021-05-16 12:54:01 +0200

    Cleanup.

--- assets/scripts/block-library/spacer/common.js
+++ assets/scripts/block-library/spacer/common.js
@@ -4,8 +4,9 @@ import { getBreakpointInfix, getGridBreakpoints } from '../../helpers';
 
 export const getSpacerClasses = ( attributes ) => {
   const { sizes } = attributes;
+  const breakpoints = getGridBreakpoints();
   let classes = {};
-  map( getGridBreakpoints(), ( breakpoint ) => {
+  map( breakpoints, ( breakpoint ) => {
     let infix = getBreakpointInfix( breakpoint );
     let breakpointSize = get( sizes, breakpoint );
     classes[`has-spacing${infix}-${breakpointSize}`] = breakpointSize;
